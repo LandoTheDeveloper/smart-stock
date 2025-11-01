0cc5d33477c3bee40c890d93265b4a8895c925c1
GafurCodes <83255459+GafurCodes@users.noreply.github.com>
2025-11-01 13:02:26 -0400

    fix ts error

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { type FormEvent, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 
