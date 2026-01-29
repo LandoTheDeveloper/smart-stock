ba715b5d8089a94faf57336b0a9ab6f7e34ef5b4
Ryan Damel <damelr7155@gmail.com>
2026-01-28 19:14:38 -0500

    Removed the react native webapp title

--- smart-stock-mobile/app/index.tsx
+++ smart-stock-mobile/app/index.tsx
@@ -1,14 +1,9 @@
 
 import { Redirect } from "expo-router";
-import { useEffect } from 'react';
 
 import SmartStockLogo from "../../assets/SmartStockLogo.png";
 
 export default function Index() {
-
-  // On app start, change the name of the webpage to "SmartStock" and the favicon to "SmartStockLogo.png"
-  useEffect(() => {document.title = "SmartStock";}, []);
-
   // On app start, always go to the login screen
   return <Redirect href="/auth/login" />;
 }
